169b2f8cc4b310223248602d07c3e069ffbe55d7
bigmak712 <bigmak712@gmail.com>
2017-03-02 18:00:32 -0800

    Finished intro video

---
+++ InstagramDemo/LoginViewController.swift
@@ -0,0 +1,64 @@
+//
+//  LoginViewController.swift
+//  InstagramDemo
+//
+//  Created by Timothy Mak on 3/2/17.
+//  Copyright © 2017 Timothy Mak. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        PFUser.logInWithUsername(inBackground: usernameField.text!, password: passwordField.text!) { (user: PFUser?, error: Error?) in
+            if user != nil {
+                print("logged in!")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+        
+    }
+
+    @IBAction func onSignUp(_ sender: Any) {
+        let newUser = PFUser()
+        
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if success {
+                print("created a user!")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else {
+                print(error?.localizedDescription as Any)
+            }
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
