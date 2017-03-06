b17a2ef72cfae995e1b82b5dcc4f002d83027ac5
bigmak712 <bigmak712@gmail.com>
2017-03-05 18:58:22 -0800

    Can now select photos from the photo library

--- InstagramDemo/CaptureViewController.swift
+++ InstagramDemo/CaptureViewController.swift
@@ -9,19 +9,52 @@
 import UIKit
 import Parse
 
-class CaptureViewController: UIViewController {
+class CaptureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var defaultPhotoLabel: UILabel!
+    @IBOutlet weak var captionTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        let imagePickerController = UIImagePickerController()
+        
+        imagePickerController.delegate = self
+        //imagePickerController.allowsEditing = true
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Error when picking photo")
+        }
+        
+        photoImageView.image = image
+        
+        defaultPhotoLabel.isHidden = true
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+        defaultPhotoLabel.isHidden = false
+    }
 
     /*
     // MARK: - Navigation
